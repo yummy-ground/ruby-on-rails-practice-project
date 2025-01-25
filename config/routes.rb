779be8dbf3bccce43ab4c334171e0530b6373c79
yummygyudon <bang2brew@gmail.com>
2025-01-25 20:58:46 +0900

    feat : home_controller (static variable)

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   get "/index" => "home#index", as: :home
+  get "/add" => 'calculator#add', as: :calculator_add
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
